045feabb928b6bb96a0d47a8a3c074d2cd673e58
elahe <elahednyl@gmail.com>
2025-07-04 14:34:09 +0330

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,229 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "University Info System",
+    description:
+      "A Python-based university management system . inspired by the actual SAMA platform.",
+    subDescription: [
+      "Implemented student and course management features with Python.",
+      "Simulated a real-world academic system based on actual university models.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/sama.jpg",
+    tags: [
+      { id: 1, name: "Python", path: "/assets/logos/python.png" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Linear Algebra Assistant",
+    description:
+      "A web app to visualize regression lines and explore subspaces using user-provided vectors.",
+    subDescription: [
+      "Built using Flask for backend logic and HTML/CSS for UI.",
+      "Displayed regression lines based on plotted points.",
+      "Included functionality to test span/subspace of vectors.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/algebra.png",
+    tags: [
+      { id: 1, name: "Python", path: "/assets/logos/python.png" },
+      { id: 2, name: "Flask", path: "/assets/logos/flask.png" },
+      { id: 3, name: "HTML", path: "/assets/logos/html.png" },
+      { id: 4, name: "CSS", path: "/assets/logos/CSS-Logo.png" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Genetic TSP",
+    description:
+      "Python Genetic Algorithm for solving TSP with visuals.",
+    subDescription: [
+      "Used genetic operations like crossover and mutation to find optimal routes.",
+      "Visualized results using matplotlib and other plotting libraries.",
+      "Tested different population sizes and mutation rates.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/TSP.jpg",
+    tags: [
+      { id: 1, name: "Python", path: "/assets/logos/python.png" },
+      { id: 2, name: "Flask", path: "/assets/logos/flask.png" },
+      { id: 3, name: "HTML", path: "/assets/logos/html.png" },
+      { id: 4, name: "CSS", path: "/assets/logos/CSS-Logo.png" },
+    ],
+  },
+  {
+    id: 4,
+    title: "Exam Sheet Evaluator",
+    description:
+      "Python tool to score scanned B/W answer sheets using answer keys.",
+    subDescription: [
+      "Compared answers with keys to evaluate correct, wrong, and blank answers.",
+      "Calculated total scores and accuracy percentage per form.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/sheet.jpg",
+    tags: [
+      { id: 1, name: "Python", path: "/assets/logos/python.png" },
+    ],
+  },
+  {
+    id: 5,
+    title: "Book shop",
+   description:
+    "A simple bookshop system built with Python and SQLite.",
+  subDescription: [
+    "Managed books, users, and transactions using a relational database schema.",
+    "Used SQLite for lightweight and efficient data storage.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/book.jpg",
+    tags: [
+      { id: 1, name: "Python", path: "/assets/logos/python.png" },
+      { id: 2, name: "SQLite", path: "/assets/logos/sqlite.png" },
+    ],
+  },
+  {
+    id: 6,
+    title: "Regularization Experiments",
+    description:
+      "A series of deep learning experiments with different regularization techniques.",
+    subDescription: [
+      "Trained models on MNIST, IMDB, Reuters and others using Keras .",
+      "Applied dropout, L1/L2 regularization, and batch normalization.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/dl.webp",
+    tags: [
+      { id: 1, name: "Python", path: "/assets/logos/python.png" },
+    ],
+  },
+];
+
+export const mySocials = [
+    {
+    name: "Email",
+    href: "mailto:elahedaniali@outlook.com",
+    icon: "/assets/logos/email.jpeg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/elahe-daniali-168092319/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/elahe_daniali_/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+
+export const experiences = [
+  {
+    title: "Python Developer",
+    job: "Learning and Projects Since University",
+    date: "2021–Present",
+    contents: [
+      "Started learning Python in the first year of university to build simple programs.",
+      "Practiced through small projects to improve logic and problem-solving abilities.",
+      "Worked with various Python libraries including those for data analysis, visualization, and backend development.",
+      "Currently improving Python proficiency through practical use in my projects and personal work.",
+    ],
+  },
+  {
+    title: "Academic and Personal Projects",
+    job: "University of Technology, Urmia",
+    date: "2022–Present",
+    contents: [
+      "Developed a university management system inspired by the SAMA platform.",
+      "Built an exam sheet evaluator to score scanned B/W answer sheets using Python.",
+      "Created a simple bookshop system using Python and SQLite to manage books and transactions.",
+      "Performed deep learning experiments using Keras and applied various regularization techniques.",
+    ],
+  },
+  {
+    title: "Web Development Projects",
+    job: "Full-Stack Practice",
+    date: "2024–Present",
+    contents: [
+      "Learned and applied web technologies to build interactive tools.",
+      "Built a linear algebra assistant for visualizing regression lines and testing subspaces.",
+      "developed a Python-based genetic algorithm to efficiently solve the Traveling Salesman Problem, with clear visualizations of route optimizations."
+
+   ],
+  },
+  {
+    title: "Programming and Cybersecurity Competitions",
+    job: "ICPC and CTF Achievements",
+    date: "2023–2024",
+    contents: [
+      "✦ Placed 1st in the university ICPC contest.",
+      "✦ Participated in the national ICPC event held in Tehran.",
+      "✦ Secured 2nd place in the university's CTF (Capture The Flag) cybersecurity competition.",
+    ],
+  },
+];
+
+export const reviews = [
+  {
+    name: "Pardis",
+    username: "@pardis_dev",
+    body: "It was great working with Elahe. Her Python skills are solid.",
+    img: "https://robohash.org/pardis",
+  },
+  {
+    name: "Pouria",
+    username: "@pouria_codes",
+    body: "Elahe’s projects are clear and well documented. I recommend her work.",
+    img: "https://robohash.org/pouria",
+  },
+  {
+    name: "Parisa",
+    username: "@parisa_it",
+    body: "Elahe is a quick learner and a good problem solver. Always delivers quality.",
+    img: "https://robohash.org/parisa",
+  },
+  {
+    name: "Majid",
+    username: "@majid_design",
+    body: "Great web development skills! Interfaces are smooth and user friendly.",
+    img: "https://robohash.org/majid",
+  },
+  {
+    name: "Shahnaz",
+    username: "@shahnaz_cyber",
+    body: "Professional and knowledgeable in cybersecurity and backend development.",
+    img: "https://robohash.org/shahnaz",
+  },
+  {
+    name: "Viana",
+    username: "@viana_dev",
+    body: "Elahe’s deep learning experiments are well done and innovative.",
+    img: "https://robohash.org/sina",
+  },
+  {
+    name: "Sina",
+    username: "@sina_ai",
+    body: "Reliable and precise. Communication and collaboration were always clear.",
+    img: "https://robohash.org/viana",
+  },
+  {
+    name: "Tarla",
+    username: "@tarla_research",
+    body: "Elahe shows commitment and passion in every project she works on.",
+    img: "https://robohash.org/tarla",
+  },
+  {
+    name: "Sana",
+    username: "@sana_review",
+    body: "She works carefully and skillfully on projects, always aiming for the best results.",
+    img: "https://robohash.org/sana",
+  },
+];
